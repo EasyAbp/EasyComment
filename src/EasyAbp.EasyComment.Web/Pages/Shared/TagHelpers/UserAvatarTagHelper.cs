df1bad2f21417ebab11e36b6ef233f1f11d1337b
wakuflair <wakuflair@163.com>
2020-07-13 14:07:35 +0800

    Introduced `UserAvatarTagHelper`

---
+++ src/EasyAbp.EasyComment.Web/Pages/Shared/TagHelpers/UserAvatarTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EasyAbp.EasyComment.CommentUsers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace EasyAbp.EasyComment.Web.Pages.Shared.TagHelpers
+{
+    [HtmlTargetElement("img", Attributes = "comment-creator")]
+    public class UserAvatarTagHelper : TagHelper
+    {
+        public static string DefaultImageUrl = "/images/default_avatar.png";
+
+        [HtmlAttributeName("comment-creator")]
+        public CommentUserDto CommentCreator { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var imgSrc = CommentCreator.Avatar.IsNullOrEmpty() ? DefaultImageUrl : CommentCreator.Avatar;
+            output.Attributes.SetAttribute("src", imgSrc);
+        }
+    }
+}
